9fc651b8f79a7c84c17718ddd9497cd63e8494f5
Nermeen-M <nermeen.mamdouh.nm@gmail.com>
2023-02-02 14:04:33 +0100

    Package details

--- src/Pages/PackageInformation.jsx
+++ src/Pages/PackageInformation.jsx
@@ -3,11 +3,31 @@ import { useParams } from "react-router-dom";
 export default function PackageInformation({ packages }) {
   const params = useParams();
   const product = packages.find((item) => item.id == params.packageId);
+  const {
+    status,
+    eta,
+    parcel_id,
+    sender,
+    verification_required,
+    location_name,
+    notes,
+    last_updated,
+  } = product;
+  console.log(product);
 
   return (
     <div>
       Package Information
-      <div>{product.sender}</div>
+      <div>
+        <h2>{sender}</h2>
+        <span>{parcel_id}</span>
+      </div>
+      <span>{status}</span>
+      <span>{last_updated}</span>
+      <span>{location_name}</span>
+      <span>{eta}</span>
+      {notes && <span>{notes}</span>}
+      {verification_required && <span>Be sure to show your valid ID.</span>}
     </div>
   );
 }
